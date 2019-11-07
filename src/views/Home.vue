1fc672f165a1b4c0912d1a765bfa2526ff5b24ac
Isaias <johndoe@example.com>
2019-11-07 19:25:04 +0100

    home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,7 @@
   <v-layout row wrap justify-center>
     <v-flex   xs12  sm8 md6  >
       <v-card class="hovercard backg">
+        <v-toolbar-title dark>NOTICIAS</v-toolbar-title>
         <router-link :to="{name: 'noticias'}"><v-img src="http://fox-grove.com/wp-content/uploads/2014/12/black-polygon-with-red-edges-abstract-hd-wallpaper-1920x1080.jpg"
           aspect-ratio="1.9">
        
@@ -14,6 +15,7 @@
 
     <v-flex  xs12  sm8 md6 >
       <v-card class="hovercard backg">
+        <v-toolbar-title dark>PREMIOS</v-toolbar-title>
         <router-link :to="{name: 'juegos'}"><v-img src="http://fox-grove.com/wp-content/uploads/2014/12/black-polygon-with-red-edges-abstract-hd-wallpaper-1920x1080.jpg"
           aspect-ratio="1.9">
        
@@ -25,6 +27,7 @@
 
     <v-flex  xs12  sm8 md6  >
       <v-card class="hovercard backg">
+        <v-toolbar-title dark>QUIENES SOMOS</v-toolbar-title>
         <router-link :to="{name: 'about'}"><v-img src="http://fox-grove.com/wp-content/uploads/2014/12/black-polygon-with-red-edges-abstract-hd-wallpaper-1920x1080.jpg"
           aspect-ratio="1.9">
         
@@ -36,6 +39,7 @@
 
     <v-flex  xs12  sm8 md6 >
       <v-card class="hovercard backg">
+        <v-toolbar-title dark>REGISTRO</v-toolbar-title>
         <router-link :to="{name: 'register'}"><v-img src="http://fox-grove.com/wp-content/uploads/2014/12/black-polygon-with-red-edges-abstract-hd-wallpaper-1920x1080.jpg"
           aspect-ratio="1.9">
      
